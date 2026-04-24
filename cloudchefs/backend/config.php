c891bb6d08d0d696742c90dc2e9d168ba29ff3a0
Mateus Juil Vargas Bento <106758970+Juilcode22@users.noreply.github.com>
2026-04-24 20:29:24 -0300

    Update config.php

--- cloudchefs/backend/config.php
+++ cloudchefs/backend/config.php
@@ -1,7 +1,7 @@
 <?php
-$DB_HOST = "localhost";
+$DB_HOST = "dbcloudchefs.cbo86kscekku.us-east-2.rds.amazonaws.com";
 $DB_PORT = "5432";
 $DB_USER = "postgres";
-$DB_PASS = "teste123"; // sua senha real
-$DB_NAME = "cloudchefs";
+$DB_PASS = "IGJA0cUOUy6ymHefxrfL"; // sua senha real
+$DB_NAME = "postgres";
 ?>
